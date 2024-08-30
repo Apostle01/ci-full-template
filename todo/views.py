3a88ab8fe5361b8aa3eea3aa22d4ac5e858fe659
Apostle01 <nanaafianyamekye2@gmail.com>
2024-08-30 10:07:54 +0000

    modified todo/views.py todo/models.py todo/admin.py

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import TodoItem
+from .models import Item
 from .forms import ItemForm  # Add this line to import ItemForm
 
 def get_todo_list(request):
