9b184565455d5f70b2e410ca82836bdd2ac60965
Apostle01 <nanaafianyamekye2@gmail.com>
2024-08-30 09:51:30 +0000

    modified todo/admin.py todo/models.py todo/views.py

--- todo/models.py
+++ todo/models.py
@@ -2,9 +2,20 @@ from django.db import models
 
 
 # Create your models here.
-class Item(models.Model):
+class TodoItem(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False) 
     done = models.BooleanField(null=False,  blank=False, default=False)
 
     def __str__(self):
       return self.name
+
+# # todo/models.py
+
+# from django.db import models
+
+# class TodoItem(models.Model):
+#     name = models.CharField(max_length=255)
+#     done = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.name
